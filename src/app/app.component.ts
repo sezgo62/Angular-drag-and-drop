76fc31666402cd6f9f2b5874e92e1f32914bf15b
Sezgin Gündogdu <sgbewerbung@yahoo.de>
2023-10-10 18:49:31 +0200

    %

---
+++ src/app/app.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular-drag-and-drop';
+
+  fruits = ['banana', 'apple', 'pear', 'tangerines'];
+
+  storageBox = [];
+
+
+  /*drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.fruits, event.previousIndex, event.currentIndex);
+  }*/
+
+
+myIndex: any;
+
+  drop(event: CdkDragDrop<string[]>) {
+    console.log('step 1 cleared');
+debugger;
+    if (event.previousContainer.data === this.fruits && event.container.data === this.storageBox) {
+      
+      // The item was moved from fruits to storageBox
+      transferArrayItem(event.previousContainer.data,
+
+                        event.container.data,
+
+                        event.previousIndex,
+
+                        event.currentIndex);
+                        debugger;
+                        this.myIndex = event.previousIndex;
+    } else if (event.previousContainer.data === this.storageBox && event.container.data === this.fruits) {
+      // The item was moved from storageBox to fruits
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+    } else {
+      // The item was moved within the same container
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    }
+  }
+  
+  
+}
